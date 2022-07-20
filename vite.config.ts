e5ef386595d3934e88ce9203b1083519baaad63a
Gustavo Maronato <gustavo@maronato.dev>
2022-07-20 01:04:28 -0300

    use mck-medicos base url

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,23 @@
 import path from "path"
 
 import react from "@vitejs/plugin-react"
-import { defineConfig } from "vite"
+import { defineConfig, UserConfig } from "vite"
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    headers: {
-      "Accept-Ranges": "bytes",
+export default defineConfig(({ command }) => {
+  const config: UserConfig = {
+    plugins: [react()],
+    server: {
+      headers: {
+        "Accept-Ranges": "bytes",
+      },
     },
-  },
-  resolve: {
-    alias: [{ find: "@", replacement: path.resolve(__dirname, "src") }],
-  },
+    resolve: {
+      alias: [{ find: "@", replacement: path.resolve(__dirname, "src") }],
+    },
+  }
+  if (command === "build") {
+    config.base = "/mck-medicos/"
+  }
+  return config
 })
